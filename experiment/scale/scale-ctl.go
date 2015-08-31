80d768de8926a33bd3a98ec4da372d79d5bdfe26
Drnutsu <dentistnut@gmail.com>
2015-09-01 00:17:06 +0700

    try to scale pod ,still find solution to observe pod resource usage

--- experiment/scale/scale-ctl.go
+++ experiment/scale/scale-ctl.go
@@ -6,6 +6,72 @@ import (
 	"regexp"
 )
 
+type metricsController interface {
+	getThreshold() int
+	setThreshold(metrics int) bool
+	checkMeetThreshold(current int) bool
+}
+
+type latencyMet struct {
+	latencyThreshold int
+}
+
+type memMet struct {
+	// TODO : type should be uint64
+	memThreshold int
+}
+
+type cpuMet struct {
+	// TODO : cpu type should be float64
+	cpuThreshold int
+}
+
+// implement PID Controller
+// Latency implemented
+func (l latencyMet) setThreshold(metrics int) bool {
+	l.latencyThreshold = metrics
+	// TODO : return error if have somethings wrong
+	return true
+}
+func (l latencyMet) getThreshold() int {
+	return l.latencyThreshold
+}
+func (l latencyMet) checkMeetThreshold(current int) bool {
+	return l.latencyThreshold == current
+}
+
+// Memory Resource implemented
+func (rm memMet) setThreshold(metrics int) bool {
+	rm.memThreshold = metrics
+	// TODO : return error if have somethings wrong
+	return true
+}
+func (rm memMet) getThreshold() int {
+	return rm.memThreshold
+}
+func (rm memMet) checkMeetThreshold(current int) bool {
+	return rm.memThreshold == current
+}
+
+// CPU Resource implemented
+func (rc cpuMet) getThreshold() int {
+	return rc.cpuThreshold
+}
+func (rc cpuMet) setThreshold(metrics int) bool {
+	rc.cpuThreshold = metrics
+	// TODO : return error if have somethings wrong
+	return true
+}
+func (rc cpuMet) checkMeetThreshold(current int) bool {
+	return rc.cpuThreshold == current
+}
+
+func testScale(m metricsController) {
+	// TODO : delete below line it's just for test
+	fmt.Println(m)
+	fmt.Println("Threshold : ", m.getThreshold())
+}
+
 func main() {
 	fmt.Println("welcome to kubenetes scale-out experiment ... ")
 	scale_num := "1"
@@ -16,6 +82,8 @@ func main() {
 	re := regexp.MustCompile("nginx-controller")
 	rc_exist := re.FindString(string(rc_stat))
 	fmt.Println(string(rc_exist))
+
+	//  create new nginx rc if not created
 	if rc_exist == "" {
 		yml_file := ""
 		fmt.Println("you don't have any nginx rc waiting for a min to create..")
@@ -28,14 +96,34 @@ func main() {
 		} else {
 			fmt.Println("failed to create nginx rc")
 		}
-
 	} else {
 		fmt.Println("you already have nginx rc ... ")
 	}
 
+	// call scale command from kubectl
 	fmt.Printf("replicas : ")
 	fmt.Scanf("%s", &scale_num)
 	cmd, _ := exec.Command("kubectl", "scale", "--replicas="+scale_num, "rc", "nginx-controller").Output()
-	//cmd, _ := exec.Command("echo", "test").Output()
 	fmt.Println(string(cmd))
+
+	// set Threshold
+	metric_type := 1
+	fmt.Printf("\n Which metrics do you want to using ? \n")
+	fmt.Printf("1. Latency \n")
+	fmt.Printf("2. Cpu Usage \n")
+	fmt.Printf("3. Memory Usage \n")
+	fmt.Scanf("%d", &metric_type)
+	// TODO : catch invalid choice value
+
+	metric_value := 0
+	fmt.Printf("value = ")
+	fmt.Scanf("%d", &metric_value)
+	// TODO : catch invalid value
+
+	// Sample object for testing
+	rm := memMet{memThreshold: metric_value}
+	testScale(rm)
+
+	// looping for check resource usage limits
+
 }
