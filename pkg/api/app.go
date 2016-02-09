ddfc385118b2a50324481da30a41aaae14a282b1
drnutsu <dentistnut@gmail.com>
2016-02-09 15:07:42 +0700

    added respone 4xx 5xx 2xx

--- pkg/api/app.go
+++ pkg/api/app.go
@@ -11,6 +11,9 @@ type AppMetric struct {
 	Memory    []*docker.CgroupMemStat `json:"memory"`
 	Request	  int64
 	Response  int64
+	Response2xx int64
+	Response4xx int64
+	Response5xx int64
 }
 
 // This is schema of Application Profile
