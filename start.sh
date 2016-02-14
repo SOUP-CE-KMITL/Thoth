7c9d74497cb02ce1e15e367507f1028e5c5f33fc
jigkoxsee <ked.naja@gmail.com>
2016-02-15 04:17:53 +0700

    add sleep to start up

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 ./setup/kube_start.sh
 ./setup/influx_start.sh
 ./setup/haproxy/vamp_start.sh
+sleep 10
 http POST localhost:10001/v1/routes < setup/haproxy/vamp_config.json
 http POST localhost:10001/v1/routes < setup/haproxy/vamp_config2.json
