2c74c96dcb0fc1caa456eb7d1bddea582c789b32
n1tingale <n.thanaphon.soo@gmail.com>
2016-03-06 21:09:52 +0700

    finish fetching from api

--- thothEyes/public/js/thothEyes.js
+++ thothEyes/public/js/thothEyes.js
@@ -85,7 +85,7 @@ angular.module('thothEyes', ['nvd3'])
 .controller('NodesCtrl', ['$scope', '$http', '$q', function($scope, $http, $q){
   $scope.testing = "testing";
   //for contain all nodes.
-  nodes = ['thotheyes.cloudapp.net'];
+  nodes = ['161.246.6.235'];
   node_datas = [];
   $scope.nodes = [];
 
@@ -136,13 +136,11 @@ angular.module('thothEyes', ['nvd3'])
     }
 
     //set initial mock data, going to remove after connect to API.
-
-
     var t = 0;
 
     setInterval(function(){
       for(var c = 0; c < nodes.length; c++){
-       node_datas[c] = $http.get("http://"+nodes[c]+":8182/metrics");
+       node_datas[c] = $http.get("https://"+nodes[c]+"/metrics");
       }
 
       $q.all(node_datas).then(function(response){
@@ -153,58 +151,130 @@ angular.module('thothEyes', ['nvd3'])
          //console.log("cpu : "+$scope.apps[r].data[1].values.slice(-1)[0].x);
          if($scope.nodes[c].data[0].values.length > 20) $scope.nodes[c].data[0].values.shift();
          if($scope.nodes[c].data[1].values.length > 20) $scope.nodes[c].data[1].values.shift();
-         t++;
+         t = t+5;
         }
        console.log($scope.nodes[0]);
       });;
       
-    }, 1000);
+    }, 5000);
 
   }])
 .controller('NodeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q){
   $scope.node = {}
-  $scope.node.name = "thotheyes.cloudapp.net"
+  $scope.node.name = "161.246.6.235"
   $scope.node.cpu = [{
     values: [],
     key: 'cpu',
     color: '#ff7f0e'
   }];
+  $scope.node.memory = [{
+    values: [],
+    key: 'memory',
+    color: '#ff7f0e'
+  }]
+  $scope.apps = [];
+  appResourcePromises =  []
 
-    $scope.resourceOptions = {
-      chart: {
-        type: 'lineChart',
-        height: 190,
-        margin: {
-          left: 100
+  $scope.cpuOptions = {
+    chart: {
+      type: 'lineChart',
+      height: 190,
+      margin: {
+        left: 100
+      },
+      duration: 0,
+      yDomain: [0,100],
+      x: function(d){ return d.x },
+      y: function(d){ return d.y },
+      useInteractiveGuideline: true,
+      xAxis: {
+        axisLabel: 'Time (s)'
+      },
+      yAxis: {
+        axisLabel: 'Resource (%)',
+        tickFormat: function(d){
+          return d3.format('.02f')(d);
         },
-        duration: 0,
-        yDomain: [0,100],
-        x: function(d){ return d.x },
-        y: function(d){ return d.y },
-        useInteractiveGuideline: true,
-        xAxis: {
-          axisLabel: 'Time (s)'
-        },
-        yAxis: {
-          axisLabel: 'Resource (%)',
-          tickFormat: function(d){
-            return d3.format('.02f')(d);
-          },
-          axisLabelDistance: -10
+        axisLabelDistance: -10
+      }
+    }
+  }
+
+  $scope.memoryOptions = angular.copy($scope.cpuOptions);
+  $scope.appOptions = {
+    chart: {
+      type: 'multiBarHorizontalChart',
+      x: function(d){return d.label;},
+      y: function(d){return d.value;},
+      margin: {
+        top: 30,
+        rigth: 20,
+        bottom: 50,
+        left: 60
+      },
+      showValues: true,
+      height: 200,
+      groupSpacing: 0.5,
+      tooltips: false,
+      showLegend: false,
+      showControls: false,
+      forceY: [0, 100],
+      yAxis: {
+        tickFormat: function(d){
+          return d3.format(',.2f')(d);
         }
       }
     }
-    var t = 0;
-  setInterval(function(){
+  }
 
-    $http.get("http://"+ $scope.node.name+":8182/metrics").then(
+  var t = 1;
+  function getNodeResource(){
+    $http.get("https://"+ $scope.node.name+"/metrics").then(
       function(response){
         $scope.node.cpu[0].values.push({ x:t, y: response.data.cpu[0]});
-       if($scope.node.cpu[0].values.length > 20) $scope.node.cpu[0].values.shift();
+        $scope.node.memory[0].values.push({ x:t, y:response.data.memory.used_percent});
+        if($scope.node.cpu[0].values.length > 20) $scope.node.cpu[0].values.shift();
+        if($scope.node.memory[0].values.length > 20) $scope.node.memory[0].values.shift();
       },
       function(err){
         console.log(err)
       });
-      t++;
-  }, 1000);
+  }
+
+  function getAppsResource(){
+      for(var c = 0; c < $scope.apps.length; c++){
+        appResourcePromises[c] = $http.get("https://" + $scope.node.name + "/app/" + $scope.apps[c].key + "/metrics/thoth");
+      }
+
+      $q.all(appResourcePromises).then(function(response){
+        for(var c=0; c < response.length; c++){
+          $scope.apps[c].values[0] = {label: "CPU",  value: response[c].data.cpu};
+          $scope.apps[c].values[1] = {label: "memory",  value: response[c].data.memory};
+        }
+        console.log("apps after resolve");
+        console.log($scope.apps); });;
+  }
+
+  function getApps(){
+    $http.get("https://"+ $scope.node.name+"/apps/thoth").then(
+      function(response){
+        for(var i=0; i < response.data.items.length; i++) {
+          $scope.apps.push({key: response.data.items[i].spec.template.metadata.name, color: "#d62728", values: []});
+        }
+        getAppsResource();
+      },
+      function(err){
+        console.log(err)
+      });
+  }
+
+
+  getNodeResource();
+  getApps();
+
+  setInterval(function(){
+    getNodeResource();
+    getAppsResource();
+    t = t+5;
+  }, 5000);
   }]);
